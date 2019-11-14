2c3a330855e0da958d7aaed184feb3d284348149
simon majirsky <s.majirsky@gmail.com>
2019-11-14 22:55:36 +0100

    4

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,4 +3,14 @@
 @section('content')
 <h1>hello</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto aut beatae esse eum expedita fugiat in iure, magnam magni optio quam qui rerum sunt temporibus tenetur totam veniam? Minima?</p>
+
+<div id="app">
+   <div class="container">
+       <articles>
+
+       </articles>
+   </div>
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
 @endsection
